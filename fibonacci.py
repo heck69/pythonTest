fce04093aae131b17c908cde9dfece322372ff44
Sharan <srikarsharan@gmail.com>
2025-06-19 21:20:00 +0530

    fibonacci series

---
+++ fibonacci.py
@@ -0,0 +1,18 @@
+def fibonacci(n):
+    if n < 0:
+        raise ValueError("should be greater than or equal to 0")
+    if n ==0:
+        return 0
+    elif n ==1:
+        return 1
+    else:
+        a = 0
+        b = 1
+        for i in range(2, n+2):
+            print(a, end=" ")
+            a, b = b, a + b
+              
+    
+
+x = int(input("Enter a non-negative integer: "))
+result = fibonacci(x)
